5b855428eae245e1b20b82cd057bc0665ef13597
ImRieul <rieul.im@gmail.com>
2022-06-27 01:11:43 +0900

    route 1

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.css'
 
 import '../css/App.css';
@@ -15,25 +16,30 @@ function App() {
         <Container>
         <Navbar.Brand href="#home">Shop</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#cart">Cart</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/detail">Cart</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
       
-      <div className="main-bg"></div>
-      
-      <div className="container">
-        <div className="row">
-          {
-            items.map( (value) => {
-              return( 
-                <ItemList item={ value } key={value.id}></ItemList>
-              )
-            })
-          }
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="main-bg"></div>
+            <div className="container">
+              <div className="row">
+                {
+                  items.map( (value) => {
+                    return( 
+                      <ItemList item={ value } key={value.id}></ItemList>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </>
+        } />
+      </Routes>
 
     </div>
   );
