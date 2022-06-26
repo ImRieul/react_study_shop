70477fbc79584a748ccffceda1fcdee38d06c49d
mojja-im <82865792+ImRieul@users.noreply.github.com>
2022-06-27 04:23:36 +0900

    transition

--- src/components/Item_detail.js
+++ src/components/Item_detail.js
@@ -1,46 +1,23 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import styled from 'styled-components';
 
 function Detail(props) {
   let {id} = useParams();
   let item = props.item.find(x => x.id == id)
-  let [view, setView] = useState(true);  // useEffect 1
-  let [count, setCount] = useState(0);  // useEffect 1
-  let [policy, setPolicy] = useState(''); // useEffect 2
-
-  // html 로드 된 이후 실행
-  // useEffect(Callback, variable or state)
-  // 2번째 매개변수에 아무 값도 입력하지 않으면 계속 실행, []를 넣으면 1번만 실행
-  // 특정 변수를 넣으면 그 변수가 변경될 때만 싫행
-  useEffect(()=>{
-    // run if component load or update
-    // after start
-    console.log('안녕');
-    console.log(count);
-    let hide = setTimeout(() => {
-      setView(false);
-    }, 2000)
-
-
-
-    return () => {
-      // useEffect의 2번째 매개변수에 []를 넣으면 unmonut시 1회 실행
-      // before start
-      clearTimeout(hide);
-    }
-  });
+  let [tab, setTab] = useState(0);  // tab UI
+  let [animation, setAnimation] = useState('start')  // tab UI
 
   useEffect(() => {
-    const reg = new RegExp('[0-9]');
-    if (reg.test(policy)) {
-      alert('NO NUMBER');
-    } 
-  }, [policy])
-
+    setTimeout(() => { setAnimation('start end') }, 100);
+    return (
+      setAnimation('start')
+    )
+  }, [])
 
   return (
-    <div className="container">
+    <div className={`container ${animation}`}>
       <div className="row">
         <div className="col-md-6">
           <img src={ item.img } width="100%" />
@@ -52,12 +29,20 @@ function Detail(props) {
           <button className="btn btn-danger">Flex</button> 
         </div>
       </div>
-      {
-        // styled-components
-        view? <YellowBtn id="yellowBtn">Yellow Button</YellowBtn>: null
-      }
-      { view? <button onClick={() => { setCount(count+1) } }>Count</button> : null }
-      <input onChange={ (event) => { setPolicy(event.target.value)}} />
+
+      <Nav variant="tabs"  defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link onClick={() => { setTab(0) }} eventKey="link0">버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={() => { setTab(1) }} eventKey="link1">버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={() => { setTab(2) }} eventKey="link2">버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <TabContent tab={tab} item={item}></TabContent>
     </div> 
   )
 }
@@ -65,11 +50,19 @@ function Detail(props) {
 export default Detail;
 
 
-// styled-components
-// css 기능을 js로 할 수 있음, 굳이 써야 할 이유는 모르겠음.
-let YellowBtn = styled.div` 
-  background : ${ props => props.bg };
-  color : ${ props => props.bg == 'blue' ? 'white' : 'black' };
-  padding : 10px; 
-  background-color : yellow;
-`; 
+function TabContent(props) {
+  let [animation, setAnimation] = useState('start')
+
+  useEffect(() => {
+    setTimeout(() => { setAnimation('start end') }, 100);
+    return (
+      setAnimation('start')
+    )
+  }, [props.tab])
+
+  return (
+    <div className={animation}>
+      { [<div>content 0</div>, <div>content 1</div>, <div>content 2</div>][props.tab] }
+    </div>
+  )
+}
